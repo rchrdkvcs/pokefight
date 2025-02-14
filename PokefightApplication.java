06c23344364397547f9f6ba6c46551c4e8e78ad2
rchrdkvcs <b34r.rk@gmail.com>
2025-02-15 00:14:22 +0100

    fix(app): missing PokerfightApplication.java

---
+++ PokefightApplication.java
@@ -0,0 +1,13 @@
+package org.example.pokefight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokefightApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PokefightApplication.class, args);
+  }
+
+}
---
+++ src/main/java/org/example/pokefight/PokerfightApplication.java
@@ -0,0 +1,12 @@
+package org.example.pokefight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class PokefightApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PokefightApplication.class, args);
+  }
+}

ae27a96fcf59234d8034437a9e64f15476944386
rchrdkvcs <b34r.rk@gmail.com>
2025-02-15 00:01:41 +0100

    chore: spring-boot project init

---
+++ src/test/java/org/example/pokefight/PokefightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pokefight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokefightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
